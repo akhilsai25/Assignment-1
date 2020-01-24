3bf7855a812adf785ede8823c3b084458b8b2dc1
akhilsai25 <38877327+akhilsai25@users.noreply.github.com>
2020-01-24 10:44:28 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String name=" ";
         if(args.length==0)
         {
                 System.out.println("Please enter the name of person. ");
